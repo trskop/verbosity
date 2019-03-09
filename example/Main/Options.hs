d5f5a44e4d0c49fea25f962be67f29b5222cb215
Peter Trško <peter.trsko@gmail.com>
2019-03-09 15:57:12 +0000

    Including example in README; bumped LTS 13 to 13.11

--- example/Main/Options.hs
+++ example/Main/Options.hs
@@ -1,20 +1,3 @@
-# verbosity
-
-[![Hackage](http://img.shields.io/hackage/v/verbosity.svg)][Hackage: verbosity]
-[![Hackage Dependencies](https://img.shields.io/hackage-deps/v/verbosity.svg)](http://packdeps.haskellers.com/feed?needle=verbosity)
-[![Haskell Programming Language](https://img.shields.io/badge/language-Haskell-blue.svg)][Haskell.org]
-[![BSD3 License](http://img.shields.io/badge/license-BSD3-brightgreen.svg)][tl;dr Legal: BSD3]
-
-[![Build](https://travis-ci.org/trskop/verbosity.svg)](https://travis-ci.org/trskop/verbosity)
-
-
-## Description
-
-Simple enum that encodes application verbosity with various useful instances.
-
-## Example
-
-```Haskell
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
@@ -70,8 +53,3 @@ incrementVerbosityFlag =
         [ Options.short 'v'
         , Options.help "Increment verbosity by one level. Can be used multiple times."
         ]
-```
-
-[Hackage: verbosity]: http://hackage.haskell.org/package/verbosity "verbosity package on Hackage"
-[Haskell.org]: http://www.haskell.org "The Haskell Programming Language"
-[tl;dr Legal: BSD3]: https://tldrlegal.com/license/bsd-3-clause-license-%28revised%29 "BSD 3-Clause License (Revised)"
